b34feef45ee2f9715cdf3370167b0df7b33e94a1
SyedUmerHasan <umerhasan2010@gmail.com>
2019-06-11 10:31:07 +0500

    adding component folder and other materials

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import SecondBanner from './Components/SecondBanner/SecondBanner';
+import ThirdBanner from './Components/ThirdBanner/ThirdBanner';
+import FourBanner from './Components/FourBanner/FourBanner';
+import FiveBanner from './Components/FiveBanner/FiveBanner';
+import SixBanner from './Components/SixBanner/SixBanner';
+import Footer from './Components/Footer/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar/>
+        <SecondBanner data={"I am umer"}/>
+        <ThirdBanner/>
+        <FourBanner/>
+        <FiveBanner/>
+        <SixBanner/>
+        <Footer/>
+      </div>
+    );
+  }
 }
+
 
 export default App;
